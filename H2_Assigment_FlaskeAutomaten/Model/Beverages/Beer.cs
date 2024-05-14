cda6a8e2a1d69c3e5bb310a8d6429ca0a0fcbc37
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-05-14 13:30:08 +0200

    Added classes yes

---
+++ H2_Assigment_FlaskeAutomaten/Model/Beverages/Beer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Assigment_FlaskeAutomaten.Model.Beverages
+{
+    internal class Beer : Beverage
+    {
+        private float _alchohol;
+
+        public Beer(float alchohol) : base("Jack & Beer")
+        {
+            _alchohol = alchohol;
+        }
+
+        public float Alchohol
+        {
+            get
+            {
+                return _alchohol;
+            }
+        }
+    }
+}
---
+++ H2_Assigment_FlaskeAutomaten/Model/Machines/Splitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Assigment_FlaskeAutomaten.Model.Machines
+{
+    internal class Splitter : Machine
+    {
+        private Conveyor _inputConveyor;
+        private Conveyor[] _outputConveyor = new Conveyor[2];
+
+        public Splitter(Conveyor inputConveyor, Conveyor[] outputConveyors)
+        {
+            _inputConveyor = inputConveyor;
+            _outputConveyor = outputConveyors;
+        }
+    }
+}

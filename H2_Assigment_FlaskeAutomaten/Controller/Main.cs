4b5a902e0e3c6191de2b349798597d930ea6e81a
janickelev@gmail.com <janickelev@gmail.com>
2024-05-14 23:02:56 +0200

    Spiltter update

--- H2_Assigment_FlaskeAutomaten/Controller/Main.cs
+++ H2_Assigment_FlaskeAutomaten/Controller/Main.cs
@@ -12,13 +12,17 @@ namespace H2_Assigment_FlaskeAutomaten.Controller
 {
     internal class Main
     {
-        private Conveyor _inputConveyor = new Conveyor();
-        private Conveyor[] _outputConveyors = { new Conveyor(), new Conveyor() };
+        public static Conveyor inputConveyor = new Conveyor();
+        public static Conveyor[] outputConveyors = { new Conveyor(), new Conveyor() };
         public void Setup()
         {
-            _inputConveyor.AddToInventory(new Soda(100));
+            inputConveyor.AddToInventory(new Soda(100));
+            inputConveyor.AddToInventory(new Soda(100));
+            inputConveyor.AddToInventory(new Soda(100));
+            inputConveyor.AddToInventory(new Beer(10));
+            inputConveyor.AddToInventory(new Beer(10));
             Splitter splitter = SetupNewSplitter();
-			Thread thread = new Thread(splitter.Sort);
+			Thread thread = new Thread(splitter.Start);
             thread.Start();
 
 //			Console.WriteLine(splitter.Inventory[0].Name);
@@ -28,7 +32,7 @@ namespace H2_Assigment_FlaskeAutomaten.Controller
         private Splitter SetupNewSplitter()
 		{
 
-			return new Splitter(_inputConveyor, _outputConveyors);
+			return new Splitter(inputConveyor, outputConveyors);
 		}
 
 	}

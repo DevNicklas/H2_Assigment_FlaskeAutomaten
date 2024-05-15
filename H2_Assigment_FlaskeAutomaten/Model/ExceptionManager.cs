d117264ba54fad75fb48ced61bf1709671d9bf52
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-05-15 08:52:56 +0200

    s

--- H2_Assigment_FlaskeAutomaten/Model/ExceptionManager.cs
+++ H2_Assigment_FlaskeAutomaten/Model/ExceptionManager.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace H2_Assigment_FlaskeAutomaten.Model
 {
     /// <summary>
-    /// Manages all custom exceptions, and controls messages that will be shown for the user
+    /// Manages exceptions, and controls messages that will be shown for the user
     /// </summary>
     internal class ExceptionManager
     {

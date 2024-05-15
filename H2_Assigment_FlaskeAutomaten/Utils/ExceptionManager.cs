fd7af1ec86e54dce62f257c601763b0f43794aec
Jonas Steinmejer <jonas04@outlook.dk>
2024-05-15 11:29:59 +0200

    Moved ExceptionManager.cs to Utils folder

---
+++ H2_Assigment_FlaskeAutomaten/Utils/ExceptionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace H2_Assigment_FlaskeAutomaten.Utils
+{
+    /// <summary>
+    /// Manages exceptions, and controls messages that will be shown for the user
+    /// </summary>
+    internal class ExceptionManager
+    {
+    }
+}

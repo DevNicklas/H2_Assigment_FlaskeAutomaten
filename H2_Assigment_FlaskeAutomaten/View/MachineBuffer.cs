24ae327a6f982157150b60caafd7486e6280fb25
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2024-05-15 12:21:06 +0200

    Noget sejt er addet

--- H2_Assigment_FlaskeAutomaten/View/MachineBuffer.cs
+++ H2_Assigment_FlaskeAutomaten/View/MachineBuffer.cs
@@ -8,7 +8,10 @@ using System.Windows.Forms;
 
 namespace H2_Assigment_FlaskeAutomaten.View
 {
-    internal class MachineBuffer
+	/// <summary>
+	/// Represents a buffer for holding beverages in a beverage vending machine.
+	/// </summary>
+	internal class MachineBuffer
     {
         private ListBox _listBoxBeverages;
         private ProgressBar _beverageListProgress;
@@ -18,11 +21,12 @@ namespace H2_Assigment_FlaskeAutomaten.View
             _listBoxBeverages = listBoxBeverages;
             _beverageListProgress = beverageListProgress;
         }
-
+		// Method to add a beverage to the list box and update the progress bar
 		internal void AddBeverage(Beverage beverage)
 		{
 			if (_listBoxBeverages.Items.Count < _beverageListProgress.Maximum)
 			{
+				// If invoking is required (cross-thread operation), invoke adding beverage again
 				if (_listBoxBeverages.InvokeRequired)
 				{
 					_listBoxBeverages.Invoke(new Action(() => AddBeverage(beverage)));
@@ -35,6 +39,7 @@ namespace H2_Assigment_FlaskeAutomaten.View
 			}
 		}
 
+		// Method to remove a beverage from the list box and update the progress bar
 		internal void RemoveBeverage(Beverage beverage)
 		{
 			if (_listBoxBeverages.InvokeRequired)
@@ -43,12 +48,14 @@ namespace H2_Assigment_FlaskeAutomaten.View
 			}
 			else
 			{
+				// If the beverage is not found in the list box, throw an exception
 				if (!_listBoxBeverages.Items.Contains(beverage.Name))
 				{
 					throw new NotImplementedException();
 				}
 				else
 				{
+					// Remove the beverage from the list box and update the progress bar
 					_listBoxBeverages.Items.Remove(beverage.Name);
 					_beverageListProgress.Value--;
 				}
